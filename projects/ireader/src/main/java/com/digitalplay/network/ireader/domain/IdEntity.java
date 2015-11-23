7b897cf663d8fb5ac20fa39af84fce2f7cfd411d
eeyesonme <eeyesonme@gmail.com>
2015-11-24 00:58:00 +0800

    22

---
+++ projects/ireader/src/main/java/com/digitalplay/network/ireader/domain/IdEntity.java
@@ -0,0 +1,14 @@
+package com.digitalplay.network.ireader.domain;
+
+public abstract class IdEntity {
+
+	protected Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
