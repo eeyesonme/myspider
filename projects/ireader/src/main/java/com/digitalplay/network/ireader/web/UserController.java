42d128aa799998a90522c1707a9d21524d937170
Qiao <hoqiao@coriant.com>
2016-12-02 17:02:48 +0800

    S

--- projects/ireader/src/main/java/com/digitalplay/network/ireader/web/UserController.java
+++ projects/ireader/src/main/java/com/digitalplay/network/ireader/web/UserController.java
@@ -5,11 +5,20 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.digitalplay.network.ireader.common.search.SearchRequest;
+import com.digitalplay.network.ireader.domain.sys.Organization;
+import com.digitalplay.network.ireader.domain.sys.Position;
 import com.digitalplay.network.ireader.domain.sys.User;
 import com.digitalplay.network.ireader.service.sys.AccountService;
 import com.digitalplay.network.ireader.web.annotation.CurrentUser;
@@ -27,7 +36,25 @@ public class UserController {
 		
 		List<User> users = userService.findAll();
 		model.addAttribute("users", users);
-		return "admin/sys/user/list";
+		return "admin/sys/user/main";
 	}
 	
+	@RequestMapping(value = {"{organization}/{position}"}, method = RequestMethod.GET)
+    public String list(@PathVariable("organization") Organization organization,
+    		@PathVariable("position") Position position,
+            SearchRequest searchRequest, Model model) {
+
+        if (organization != null && !organization.isRoot()) {
+        	searchRequest.addSearchParam("organizationId", organization.getId());
+        }
+        if (position != null && !position.isRoot()) {
+        	searchRequest.addSearchParam("positionId", position.getId());
+        }
+        
+        Pageable pageable = new PageRequest(1, 10, new Sort(Direction.ASC));
+        
+        Page<User> users =userService.findAll(searchRequest,  pageable);
+        model.addAttribute("page",users);
+        return "admin/sys/user/list";
+    }
 }
--- projects/ireader/src/main/webapp/WEB-INF/views/admin/index/welcome.jsp
+++ projects/ireader/src/main/webapp/WEB-INF/views/admin/index/welcome.jsp
@@ -32,15 +32,6 @@
         <div style="padding-left: 10px;">
             <a class="btn btn-link btn-view-info" data-toggle="tooltip" data-placement="bottom" title="点击查看个人资料/修改密码">
             </a>
-            <span class="muted">|</span>
-            &nbsp;
-            <span class="muted">
-                您有
-                <a class="btn btn-link btn-view-message no-padding" data-toggle="tooltip" data-placement="bottom" title="点击查看未读消息">
-                    <span class="badge badge-important">${messageUnreadCount}条</span>
-                </a>
-                未读消息
-            </span>
         </div>
     </div>
     <br/>
